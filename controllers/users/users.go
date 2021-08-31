122bf0320fc49c54967ea500526b731b4defb9d8
Tiago Murilo <tiago.murilo08@gmail.com>
2021-08-30 23:31:47 -0300

    Meu primeiro servidor http, para API Restful com Golang (estou gostando da tecnologia)

--- controllers/users/users.go
+++ controllers/users/users.go
@@ -1,10 +1,32 @@
 package users
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type User struct {
+	username string
+	isAdmin  bool
+	fullname string
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Success")
+	user := User{
+		fullname: "Tiago Murilo Ochôa da Luz",
+		isAdmin:  true,
+		username: "tiago.murilo",
+	}
+
+	fmt.Println(user)
+	userJson, err := json.Marshal(user)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	w.Write(userJson)
 }
